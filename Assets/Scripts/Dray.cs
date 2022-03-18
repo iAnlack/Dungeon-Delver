4ddfd38d8e15ba4246cbc6922729b6ad2ba2e430
Anlack <aka3jiouqpahat@mail.ru>
2022-03-18 16:08:46 +0300

    Добавление героя
    - Более интересный подход к реализации движения
    - Лучший способ обработки нажатых клавиш
    - Анимация движения Дрея

--- Assets/Scripts/Dray.cs
+++ Assets/Scripts/Dray.cs
@@ -10,10 +10,15 @@ public class Dray : MonoBehaviour
     [Header("Set Dynamically")]
     public int DirHeld = -1; // Направление, соответсвующее удерживаемой клавише
     private Rigidbody _rigidbody;
+    private Animator _animator;
+
+    private Vector3[] _directions = new Vector3[] { Vector3.right, Vector3.up, Vector3.left, Vector3.down };
+    private KeyCode[] _keys = new KeyCode[] { KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow };
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -37,22 +42,23 @@ public class Dray : MonoBehaviour
         }
 
         Vector3 vel = Vector3.zero;
-        switch (DirHeld)
+        // Полностью удалите инструкцию switch, бывшую здесь
+        if (DirHeld > -1)
         {
-            case 0:
-                vel = Vector3.right;
-                break;
-            case 1:
-                vel = Vector3.up;
-                break;
-            case 2:
-                vel = Vector3.left;
-                break;
-            case 3:
-                vel = Vector3.down;
-                break;
+            vel = _directions[DirHeld];
         }
 
         _rigidbody.velocity = vel * Speed;
+
+        // Анимация
+        if (DirHeld == -1)
+        {
+            _animator.speed = 0;
+        }
+        else
+        {
+            _animator.CrossFade("Dray_Walk_" + DirHeld, 0);
+            _animator.speed = 1;
+        }
     }
 }
