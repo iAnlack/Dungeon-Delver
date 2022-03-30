1ef283c17bb5fdde0ad4d814f018d9271d74ab83
Anlack <aka3jiouqpahat@mail.ru>
2022-03-30 20:31:38 +0300

    Столкновение с плитками
    - Определение столкновения с плитками

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -8,6 +8,13 @@ public class Tile : MonoBehaviour
     public int Y;
     public int TileNum;
 
+    private BoxCollider _boxCollider;
+
+    private void Awake()
+    {
+        _boxCollider = GetComponent<BoxCollider>();
+    }
+
     public void SetTile(int eX, int eY, int eTileNum = -1)
     {
         X = eX;
@@ -22,5 +29,61 @@ public class Tile : MonoBehaviour
 
         TileNum = eTileNum;
         GetComponent<SpriteRenderer>().sprite = TileCamera.SPRITES[TileNum];
+
+        SetCollider();
+    }
+
+    // Настроеить коллайдер для этой плитки
+    private void SetCollider()
+    {
+        // Получить информацию о коллайдере из Collider DelverCollisios.txt
+        _boxCollider.enabled = true;
+        char c = TileCamera.COLLISIONS[TileNum];
+        switch (c)
+        {
+            case 'S': // Вся плитка
+                _boxCollider.center = Vector3.zero;
+                _boxCollider.size = Vector3.one;
+                break;
+            case 'W': // Верхняя половина
+                _boxCollider.center = new Vector3(0, 0.25f, 0);
+                _boxCollider.size = new Vector3(1, 0.5f, 1);
+                break;
+            case 'A': // Левая половина
+                _boxCollider.center = new Vector3(-0.25f, 0, 0);
+                _boxCollider.size = new Vector3(0.5f, 1, 1);
+                break;
+            case 'D': // Правая половина
+                _boxCollider.center = new Vector3(0.25f, 0, 0);
+                _boxCollider.size = new Vector3(0.5f, 1, 1);
+                break;
+
+            // vvvvvvvv-------- Дополнительные коды --------vvvvvvvv
+            case 'Q': // Левая верхняя четверть
+                _boxCollider.center = new Vector3(-0.25f, 0.25f, 0);
+                _boxCollider.size = new Vector3(0.5f, 0.5f, 1);
+                break;
+            case 'E': // Правая верхняя четверть
+                _boxCollider.center = new Vector3(0.25f, 0.25f, 0);
+                _boxCollider.size = new Vector3(0.5f, 0.5f, 1);
+                break;
+            case 'Z': // Левая нижняя четверть
+                _boxCollider.center = new Vector3(-0.25f, -0.25f, 0);
+                _boxCollider.size = new Vector3(0.5f, 0.5f, 1);
+                break;
+            case 'X': // Нижняя половина
+                _boxCollider.center = new Vector3(0, -0.25f, 0);
+                _boxCollider.size = new Vector3(1, 0.5f, 1);
+                break;
+            case 'C': // Правая нижняя четверть
+                _boxCollider.center = new Vector3(0.25f, 0.25f, 0);
+                _boxCollider.size = new Vector3(0.5f, 0.5f, 1);
+                break;
+            // ^^^^^^^^-------- Дополнительные коды --------^^^^^^^^
+
+            default: // Всё остальное: _, |, и др.
+                _boxCollider.enabled = false;
+                break;
+        }
     }
 }
